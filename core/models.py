3b8f2090935b9207f3730931c3906563281ae177
Alejo Arteaga <alejoarte123@icloud.com>
2025-08-13 20:13:57 -0500

    Base D1

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Propiedad(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=12, decimal_places=2)
+    direccion = models.CharField(max_length=255)
+    imagen = models.ImageField(upload_to='propiedades/', blank=True, null=True)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from core.models import Propiedad
     
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def lista_propiedades(request):
+    propiedades = Propiedad.objects.all()
+    return render(request, 'propiedades.html', {'propiedades': propiedades})
