2ee9d4fdafe1345990ea397bd633c3953300cf52
Alejandro Jaramillo <ajaram1214@gmail.com>
2025-11-08 23:38:52 -0500

    Corrección del servicio de archivos multimedia y mejoras en el panel de administración

--- properties/admin.py
+++ properties/admin.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
 from .models import Propiedad, MediaPropiedad
 
-# Register your models here.
-admin.site.register(MediaPropiedad)
 
 class MediaPropiedadInline(admin.TabularInline):
     model = MediaPropiedad
     extra = 1
 
-class PropiedadAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "location", "price_cop", "price_m2_display")
-    readonly_fields = ("price_m2_display",)
-    search_fields = ("title", "location")
 
-admin.site.register(Propiedad,PropiedadAdmin)
+@admin.register(Propiedad)
+class PropiedadAdmin(admin.ModelAdmin):
+    list_display  = ("id", "title", "location", "price_cop", "price_m2_display")
+    search_fields = ("title", "location")
+    inlines       = [MediaPropiedadInline]
+    # Si quieres mantenerlo como solo lectura en el detalle:
+    readonly_fields = ("price_m2_display",)
+
+
+@admin.register(MediaPropiedad)
+class MediaPropiedadAdmin(admin.ModelAdmin):
+    list_display  = ("id", "propiedad", "tipo")
+    list_filter   = ("tipo",)        # <-- tupla
+    search_fields = ("propiedad__title", "url", "archivo")
+    ordering      = ("-id",)         # <-- tupla
